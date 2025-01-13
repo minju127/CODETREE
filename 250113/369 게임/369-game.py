28fac471ee88d71efe44a15f444965bf6e16cc18
minju127 <thdalswn7@gmail.com>
2025-01-13 17:56:07 +0900

    Title: 369 게임, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 250113/369 게임/369-game.py
+++ 250113/369 게임/369-game.py
@@ -2,7 +2,7 @@ n=input()
 for i in range(1, int(n)+1):
     if i%3==0:
         print(0, end=' ')
-    elif '3' in n or '6' in n or '9' in n:
+    elif ('3' in i) or ('6' in i) or ('9' in i):
         print(0, end=' ')
     else:
         print(i)
