e322ec4b680c737854f8bb110c64077e3728681f
minju127 <thdalswn7@gmail.com>
2025-01-13 17:20:53 +0900

    Title: 100점 도달하기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250113/100점 도달하기/reach-100-point.py
+++ 250113/100점 도달하기/reach-100-point.py
@@ -1,12 +1,12 @@
 n=int(input())
 for i in range(n, 101):
     if i>=90:
-        print('A')
+        print('A',end=' ')
     elif i>=80:
-        print('B')
+        print('B',end=' ')
     elif i>=70:
-        print('C')
+        print('C',end=' ')
     elif i>=60:
-        print('D')
+        print('D',end=' ')
     else:
-        print('F')
+        print('F',end=' ')
