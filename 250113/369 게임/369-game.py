da7b015f96b4c12e3d0bd7c46882d09a7984e699
minju127 <thdalswn7@gmail.com>
2025-01-13 17:53:15 +0900

    Title: 369 게임, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250113/369 게임/369-game.py
+++ 250113/369 게임/369-game.py
@@ -1,8 +1,8 @@
-n=int(input())
-for i in range(1, n+1):
+n=input()
+for i in range(1, int(n)+1):
     if i%3==0:
         print(0, end=' ')
-    elif ('3' or '6' or '9') in str(i):
+    elif '3' in n or '6' in n or '9' in n:
         print(0, end=' ')
     else:
-        print(i, end=' ')
+        print(i)
