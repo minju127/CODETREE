69f1cd0959c085e7caa13a1dbfcb161cb34cbce3
minju127 <thdalswn7@gmail.com>
2025-01-29 23:35:14 +0900

    Title: 숫자 삼각형, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250129/숫자 삼각형/number-triangle.py
@@ -0,0 +1,11 @@
+# for i in range(3): 
+#     for j in range(i + 1):
+#         print(j + 1, end="")
+#     print()
+
+
+n=int(input())
+for i in range(n):  #0,1,2
+    for j in range(i+1):
+        print(j+1, end=' ')
+    print()
