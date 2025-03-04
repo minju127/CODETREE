21e3655b09de4021e0e99bfa656f6be56cfa7417
minju127 <thdalswn7@gmail.com>
2025-03-04 16:33:01 +0900

    Title: 파스칼의 삼각형, Time: 72ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250304/파스칼의 삼각형/pascals-triangle.py
@@ -0,0 +1,19 @@
+n=int(input())
+
+arr=[
+    [0 for j in range(i)]
+    for i in range(1, n+1)
+]
+
+for i in range(n):
+    arr[i][0]=1
+    arr[i][i]=1
+
+for i in range(2, n):
+    for j in range(1, i):
+        arr[i][j]=arr[i-1][j-1] + arr[i-1][j]
+
+for nums in arr:
+    for num in nums:
+        print(num end=' ')
+    print()
